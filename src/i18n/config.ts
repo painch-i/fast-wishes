2d995376754974858b15180528611b3b9a5f1406
Ismaël Painchan <ismael@baseformusic.fr>
2025-08-29 09:40:22 +0200

    fix: update fallback language from 'fr' to 'en'

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,6 +1,6 @@
 export const supportedLngs = ["fr", "en", "pseudo"] as const;
 export type Locale = typeof supportedLngs[number];
-export const fallbackLng: Locale = "fr";
+export const fallbackLng: Locale = "en";
 
 export const loadLocale = async (lng: Locale) => {
   const module = await import(/* @vite-ignore */ `./locales/${lng}/common.json`);
